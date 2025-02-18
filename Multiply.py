2f232281afb6c55789ee180c04549df8b60a9804
tau31 <tau31@pm.me>
2025-02-18 17:09:06 +0100

    added python nodes

---
+++ Multiply.py
@@ -0,0 +1,10 @@
+def multiply(data):
+    """Function that summarized cols a and b"""
+
+    data["sum"] = data["a"] * data["b"]
+
+    return data
+
+
+if __env:  # indicates we are running on Taktile
+    data = add(data)
